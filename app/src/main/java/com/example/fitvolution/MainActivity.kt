e0c7aa613027361b3590c7b6e1815dc9f97d30c3
hector11tc <hector71.htc.es@gmail.com>
2023-04-23 13:57:45 +0200

    Initial commit (Bottom Navigation View with fragments finished)

---
+++ app/src/main/java/com/example/fitvolution/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.fitvolution
+
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNavigationView.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val fragment: Fragment
+        val itemId = item.itemId
+        fragment = if (itemId == R.id.workout_item) {
+            WorkoutFragment()
+        } else if (itemId == R.id.running_item) {
+            RunningFragment()
+        } else if (itemId == R.id.metrics_item) {
+            MetricsFragment()
+        } else {
+            ProfileFragment()
+        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_fragment, fragment)
+            .commit()
+        return true
+    }
+}
