aee194b53d23c7cb70c0e90418b7a3571a789311
hector11tc <hector71.htc.es@gmail.com>
2023-04-24 18:46:36 +0200

    Base de datos implementada

--- app/src/androidTest/java/com/example/fitvolution/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/fitvolution/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.fitvolution", appContext.packageName)
+        assertEquals("com.example.com.uc3m.fitvolution", appContext.packageName)
     }
 }
